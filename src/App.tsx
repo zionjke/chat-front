deea4e5fa3cf357944012eace83967e9a7491129
artem_vasiliev <vincere802@gmail.com>
2020-11-16 15:56:08 +0200

    add socket

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import io from 'socket.io-client';
 import './App.css';
 
+const socket = io('https://samurai-chat-back.herokuapp.com/');
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    useEffect(() => {
+
+    }, []);
+
+    const [messages, setMessages] = useState([
+        {id: "dawdadas", message: "Hello", user: {id: "21555fsdf", name: "Artem"}},
+        {id: "dawd4das", message: "Whats up?", user: {id: "215dza55fsdf", name: "Viktor"}}
+    ]);
+
+    const [message, setMessage] = useState('hello');
+
+    return (
+        <div className="App">
+            <div>
+                <div style={{
+                    border: "1px solid black",
+                    padding: "10px",
+                    height: "300px",
+                    width: "300px",
+                    overflowY: "scroll"
+                }}>
+                    {
+                        messages.map(m => (
+                            <div key={m.id}>
+                                <b>{m.user.name}:</b>
+                                {m.message}
+                                <hr/>
+                            </div>
+                        ))
+                    }
+                </div>
+                <textarea value={message} onChange={(e) => setMessage(e.currentTarget.value)}>
+                </textarea>
+                <button onClick={() => socket.emit('client-message-sent',message,setMessage('')) }>Send Message</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
